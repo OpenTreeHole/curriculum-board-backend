b1ffc0b750e0f26ae0fc6ddf65de8a338c357ae0
Dest1n1 <648377563@qq.com>
2022-02-26 18:59:35 +0800

    feat: new api: add course

--- blueprints/curriculum_board/api.py
+++ blueprints/curriculum_board/api.py
@@ -5,31 +5,49 @@ from sanic.exceptions import NotFound
 from sanic_ext import validate
 from sanic_ext.extensions.openapi import openapi
 from sanic_ext.extensions.openapi.definitions import RequestBody
+from tortoise import Tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 from blueprints import bp_curriculum_board
 from blueprints.auth.decorator import authorized
 from models import Review, Course
 
-new_review = pydantic_model_creator(Review, exclude=("id", "reviewer_id", "time_created", "courses"))
-review = pydantic_model_creator(Review, exclude=("courses",))
+Tortoise.init_models(["blueprints.curriculum_board.api"], "models")
+
+NewReviewPyd = pydantic_model_creator(Review, exclude=("id", "reviewer_id", "time_created", "courses"))
+ReviewPyd = pydantic_model_creator(Review, exclude=("courses",))
+
+CoursePyd = pydantic_model_creator(Course)
+NewCoursePyd = pydantic_model_creator(Course, exclude=("id", "review_list"))
 
 
-@bp_curriculum_board.post("/course/<course_id:int>/reviews")
+
+@bp_curriculum_board.post("/courses")
 @openapi.body(
-    RequestBody({"application/json": new_review.construct(title="title", content="content", rank="C+", remark=10)}))
+    RequestBody({"application/json": NewCoursePyd.construct()}))
 @authorized()
-@validate(json=new_review)
-async def add_review(request: Request, body: new_review, course_id: int):
+@validate(json=NewCoursePyd)
+async def add_course(request: Request, body: NewCoursePyd):
+    course_added = await Course.create(**body.dict())
+    print(str(CoursePyd.schema()))
+    return json((await CoursePyd.from_tortoise_orm(course_added)).dict())
+
+
+@bp_curriculum_board.post("/courses/<course_id:int>/reviews")
+@openapi.body(
+    RequestBody({"application/json": NewReviewPyd.construct(title="title", content="content", rank="C+", remark=10)}))
+@authorized()
+@validate(json=NewReviewPyd)
+async def add_review(request: Request, body: NewReviewPyd, course_id: int):
     this_course: Optional[Course] = await Course.get_or_none(id=course_id)
     if this_course is None:
         raise NotFound(f"Course with id {course_id} is not found")
 
     review_added: Review = await Review.create(**body.dict(), reviewer_id=request.ctx.user_id)
     await this_course.review_list.add(review_added)
-    return (await review.from_tortoise_orm(review_added)).json()
+    return json((await ReviewPyd.from_tortoise_orm(review_added)).dict())
 
 
-@bp_curriculum_board.get("/course/<course_id:int>/reviews")
+@bp_curriculum_board.get("/courses/<course_id:int>/reviews")
 @authorized()
 async def get_reviews(_: Request, course_id: int):
     this_course: Optional[Course] = await Course.get_or_none(id=course_id)
@@ -37,4 +55,4 @@ async def get_reviews(_: Request, course_id: int):
         raise NotFound(f"Course with id {course_id} is not found")
 
     reviews: list[Review] = await this_course.review_list.all()
-    return json([str(r) for r in reviews])
+    return json([(await ReviewPyd.from_tortoise_orm(r)).dict() for r in reviews])
