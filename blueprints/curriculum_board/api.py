8af9d44de0ed40383ec65cc4a9c99d0c6e7bd2c5
w568w <1278297578@qq.com>
2022-02-26 15:29:21 +0800

    feat: preliminary support for adding review

--- blueprints/curriculum_board/api.py
+++ blueprints/curriculum_board/api.py
@@ -1,16 +1,24 @@
-from sanic_ext.extensions.openapi import openapi
-
-from blueprints import bp_curriculum_board
-from sanic.response import json
 from sanic import Request
-
+from sanic.exceptions import NotFound
+from sanic_ext import validate
+from sanic_ext.extensions.openapi import openapi
+from tortoise.contrib.pydantic import pydantic_model_creator
+from blueprints import bp_curriculum_board
 from blueprints.auth.decorator import authorized
-from models import Review
+from models import Review, Course
 
+new_review = pydantic_model_creator(Review, exclude=("id", "reviewer_id", "time_created", "courses"))
+review = pydantic_model_creator(Review, exclude=("courses",))
 
-@bp_curriculum_board.post("/reviews")
+@bp_curriculum_board.post("/course/<course_id:int>/reviews")
 @openapi.body({"application/json": Review})
 @authorized()
-def add_review(request: Request):
-    request.json().get()
-    return json({'res': 1})
+@validate(json=new_review)
+async def add_review(request: Request, body: new_review, course_id: int):
+    this_course: Course | None = await Course.get_or_none(id=course_id)
+    if this_course is None:
+        raise NotFound(f"Course with id {course_id} is not found")
+
+    review_added: Review = await Review.create(**body.dict(), reviewer_id=request.ctx.user_id)
+    await this_course.review_list.add(review_added)
+    return (await review.from_tortoise_orm(review_added)).json()
