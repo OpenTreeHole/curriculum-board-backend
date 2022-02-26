bd41f7f20ce4ad4dfde90fd0ea5ffb68105abb42
w568w <1278297578@qq.com>
2022-02-26 15:28:20 +0800

    fix: bad type hinting for models

--- models.py
+++ models.py
@@ -1,23 +1,22 @@
 from tortoise import fields
-from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.fields import TextField, IntField, FloatField, ManyToManyRelation, DatetimeField
 from tortoise.models import Model
 
 
 class Course(Model):
-    id: IntField = fields.IntField(pk=True)
-    name: TextField = fields.TextField()
-    credit: FloatField = fields.FloatField()
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    credit = fields.FloatField()
 
     review_list: ManyToManyRelation["Review"] = fields.ManyToManyField(model_name="models.Review",
                                                                        related_name="courses")
 
 
 class Review(Model):
-    id: IntField = fields.IntField(pk=True)
-    title: TextField = fields.TextField()
-    reviewer_id: IntField = fields.IntField()
+    id = fields.IntField(pk=True)
+    title = fields.TextField()
+    reviewer_id = fields.IntField()
     time_created: DatetimeField = fields.DatetimeField(auto_now_add=True)
-    rank: TextField = fields.TextField()
-    remark: IntField = fields.IntField()
+    rank = fields.TextField()
+    remark = fields.IntField()
     courses: fields.ManyToManyRelation[Course]
